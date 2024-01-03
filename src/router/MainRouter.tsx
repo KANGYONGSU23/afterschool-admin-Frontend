abf1272ac3961d2486568db7913eea8e07dec799
김한비 <gksql2256@dsm.hs.kr>
2024-01-04 06:09:58 +0900

    feat :: 라우터에 헤더 추가

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import {
   AfterSchoolClassCreate,
   AfterSchoolClassDetail,
@@ -10,32 +9,28 @@ import {
   Login,
   NotFound,
 } from "../pages";
+import ServiceLayout from "../components/services/layouts";
 import Header from "../components/header";
-import AfterSchoolApplication from "../pages/service/afterSchool application/AfterSchoolApplication";
+
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Login />} />
-
-        <Route path="services" element={< Header/>}>
-          <Route path="after-school-class">
-            <Route path="index" element={<AfterSchoolClassList />} />
-            <Route path="create" element={<AfterSchoolClassCreate />} />
-            <Route path="manage" element={<AfterSchoolClassManage />} />
-            <Route path=":id" element={<AfterSchoolClassDetail />} />
-          </Route>
+        <Route element={<Header />}>
+          <Route path="services">
+            <Route path="after-school-class">
+              <Route path="index" element={<AfterSchoolClassList />} />
+              <Route path="create" element={<AfterSchoolClassCreate />} />
+              <Route path=":id" element={<AfterSchoolClassDetail />} />
+            </Route>
 
             <Route path="after-school-course">
               <Route index element={<AfterSchoolCourseList />} />
               <Route path="create" element={<AfterSchoolCourseCreate />} />
               <Route path=":id" element={<AfterSchoolCourseDetail />} />
             </Route>
-          </Route>
-
-          <Route path="after-school-application">
-            <Route path="application" element={<AfterSchoolApplication/>}/>
           </Route>
         </Route>
 
