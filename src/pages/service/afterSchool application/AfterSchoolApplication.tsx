00a28e5814c06fcf78032b5f452f0cdc4de6776c
김한비 <gksql2256@dsm.hs.kr>
2024-01-04 05:45:48 +0900

    feat :: 방과후 수강신청 페이지 퍼블리싱

---
+++ src/pages/service/afterSchool application/AfterSchoolApplication.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+import { ListImg } from "../../../assets";
+
+const AfterSchoolApplication = () => {
+  return (
+    <>
+      <Container>
+        <Table>
+          <img src={ListImg} alt="listImg" />
+          <Text>방과후 수강 신청하기</Text>
+        </Table>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  background-color: white;
+  padding: 0 7%;
+`;
+
+const Table = styled.div`
+  align-items: center;
+  display: flex;
+  height: 60px;
+  padding: 0 40px;
+  gap: 10px;
+  border-radius: 8px;
+  background: #0d0d0d;
+`;
+
+const Text = styled.div`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+export default AfterSchoolApplication;
